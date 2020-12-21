a099ba867a260eb7188ba88458a959389ab6374b
Simon Males <sime@sime.net.au>
2020-12-22 00:06:52 +0100

    Red tweak

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -15,7 +15,7 @@ export const Button = styled.button`
   cursor: pointer;
   padding: 8px 16px;
   font-size: 1em;
-  background-color: #ed2186;
+  background-color: red;
   color: #fff;
   border: 1px solid #af1e66;
   
